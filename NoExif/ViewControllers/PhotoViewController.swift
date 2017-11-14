1b353ac01e1f9530b4af6af2cd18407fde512fc6
JEBishop <jbishop@Js-Mac-mini.local>
2017-11-13 19:22:20 -0500

    Capture photo
    Pass photo to new VC, break EXIF data into dictionary

---
+++ NoExif/ViewControllers/PhotoViewController.swift
@@ -0,0 +1,43 @@
+//
+//  PhotoViewController.swift
+//  NoExif
+//
+//  Created by J Bishop on 2017-11-13.
+//  Copyright © 2017 Josh Bishop. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var capturedImage: UIImageView!
+    var newImage: UIImage!
+    var imageData: CFData!
+    var exifDict: NSDictionary!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        capturedImage.image = newImage
+        
+        imageData = UIImageJPEGRepresentation(newImage, 1.0)! as CFData
+        let imageSource = CGImageSourceCreateWithData(imageData, nil)
+        let exifDict = CGImageSourceCopyPropertiesAtIndex(imageSource!, 0, nil) as NSDictionary?
+        print(exifDict!)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return exifDict.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //Fix this
+        let tableCell = tableView.dequeueReusableCell(withIdentifier: "cell") ?? UITableViewCell()
+        tableCell.textLabel?.text = "Test {indexPath.row}"
+        return tableCell
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
---
+++ NoExif/ViewControllers/ViewController.swift
@@ -0,0 +1,106 @@
+//
+//  ViewController.swift
+//  NoExif
+//
+//  Created by Josh Bishop on 2017-11-13.
+//  Copyright © 2017 Josh Bishop. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var previewView: UIView!
+    @IBOutlet weak var takePhotoButton: UIButton!
+    @IBOutlet weak var libraryButton: UIButton!
+    
+    var captureSession: AVCaptureSession?
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    var capturePhotoOutput: AVCapturePhotoOutput?
+    var capturedImage: UIImage?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let captureDevice = AVCaptureDevice.default(for:AVMediaType.video)
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice!)
+            captureSession = AVCaptureSession()
+            captureSession?.addInput(input)
+            
+            capturePhotoOutput = AVCapturePhotoOutput()
+            capturePhotoOutput?.isHighResolutionCaptureEnabled = true
+            
+            captureSession?.addOutput(capturePhotoOutput!)
+        } catch {
+            print(error)
+        }
+        
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+        videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        videoPreviewLayer?.frame = view.layer.bounds
+        previewView.layer.addSublayer(videoPreviewLayer!)
+        
+        captureSession?.startRunning()
+        
+    }
+    
+    @IBAction func takePhoto(_ sender: Any) {
+        guard let capturePhotoOutput = self.capturePhotoOutput else { return }
+        let photoSettings = AVCapturePhotoSettings()
+        photoSettings.isAutoStillImageStabilizationEnabled = true
+        photoSettings.isHighResolutionPhotoEnabled = true
+        photoSettings.flashMode = .off
+        capturePhotoOutput.capturePhoto(with: photoSettings, delegate: self as AVCapturePhotoCaptureDelegate)
+        
+        
+    }
+    
+    @IBAction func unwindToThisViewController(segue: UIStoryboardSegue) {
+        
+    }
+    /*
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        var destViewController: PhotoViewController = segue.destination as! PhotoViewController
+        destViewController.newImage = capturedImage
+    }
+    */
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+extension ViewController : AVCapturePhotoCaptureDelegate {
+    func photoOutput(_ captureOutput: AVCapturePhotoOutput,
+                     didFinishProcessingPhoto photoSampleBuffer: CMSampleBuffer?,
+                     previewPhoto previewPhotoSampleBuffer: CMSampleBuffer?,
+        resolvedSettings: AVCaptureResolvedPhotoSettings,
+        bracketSettings: AVCaptureBracketedStillImageSettings?,
+        error: Error?) {
+        guard error == nil,
+            let photoSampleBuffer = photoSampleBuffer else {
+                print("Error capturing photo: \(String(describing: error))")
+                return
+        }
+        
+        // Convert photo same buffer to a jpeg image data by using AVCapturePhotoOutput
+        guard let imageData = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: photoSampleBuffer, previewPhotoSampleBuffer: previewPhotoSampleBuffer) else {
+            return
+        }
+        
+        // Initialise an UIImage with our image data
+        capturedImage = UIImage.init(data: imageData , scale: 1.0)
+        if let image = capturedImage {
+            // Save our captured image to photos album
+            //UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+            let vc : PhotoViewController! = self.storyboard!.instantiateViewController(withIdentifier: "photoViewController") as! PhotoViewController
+            vc.newImage = image
+            self.show(vc, sender: vc)
+        }
+    }
+}
+
+
